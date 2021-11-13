346fc0cc52467492041c8925f9e4227d141b7454
Jakub Łapiński <jakublapinskigh@gmail.com>
2021-11-13 23:27:46 +0100

    datatypes

---
+++ S1/js/data_types.js
@@ -0,0 +1,29 @@
+//Primitive datatypes
+
+let day = 5;
+console.log(typeof(day));
+let c;
+console.log(typeof(c));
+
+let user_name   = "Ayooo";
+console.log(typeof(user_name));
+
+let bool_1 = true;
+console.log(typeof(bool_1));
+
+console.log(day === false);
+
+
+// Undefined
+let undefined_1 = undefined;
+console.log(typeof(undefined_1));
+
+// NULL
+let a = null;
+console.log(typeof(a));
+
+// Symbol
+// Used as a unique id
+// In object stuff
+let b = Symbol;
+console.log(typeof(b));
---
+++ S1/js/operators.js
@@ -0,0 +1,11 @@
+let inbox = 10;
+let inbox2 = 20;
+
+console.log(inbox + 10);
+console.log(inbox - 10);
+console.log(inbox * 10);
+console.log(inbox / 10);
+
+// x * x * x  and so on
+console.log(inbox **3);
+console.log(inbox % 2);
