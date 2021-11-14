886f91493dd9ba56f55ac8852d6f57fd4ee48ffb
Jakub Łapiński <jakublapinskigh@gmail.com>
2021-11-14 10:46:09 +0100

    quirky yo

---
+++ S1/js/concatenation.js
@@ -0,0 +1,36 @@
+const b1 = 100;
+const b2 = 200;
+
+const b3 = b1+b2;
+
+const s = `dev`;
+
+console.log(b1+b2);
+
+console.log(s+b3);
+
+console.log(s+`lqwe`);
+
+let s1 = b1 + `200`;
+
+console.log(s1 + ` ` + typeof(s1));   
+
+const greeting = "Welcome";
+const user = " Big";
+
+console.log(greeting+user);
+
+
+const message = "Steve Jobs once said \"LIFE GOOD\" ";
+console.log(message);  
+
+const message1 = "Steve Jobs\'s quote once said \"RIDE WIFE\" ";
+console.log(message1);
+
+const message2 = `H'E'L'L'O T/H/E/R/E`
+console.log(message2);
+
+const user1 = "Xina"
+const greeting1 = `Hey there welcome to our ${user1} `;
+console.log(greeting1);
+
