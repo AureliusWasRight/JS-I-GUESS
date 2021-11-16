4458a530963ff614919f808d11e35bc416b4b1a0
Jakub Łapiński <jakublapinskigh@gmail.com>
2021-11-16 20:56:13 +0100

    added bubbling prevention and custom text to list items

--- DOM/js/eventbubbling.js
+++ DOM/js/eventbubbling.js
@@ -2,21 +2,34 @@ const button = document.querySelector("#submit");
 const todoList = document.querySelector("#todo-list");
 const todoNr = document.querySelector(".todo-nr b");
 const mainTitle = document.querySelector(".main-title");
+const nameInput = document.querySelector(".name-input");
 const items = todoList.children;
 
-button.addEventListener("click", function () {
+button.addEventListener("click", function (event) {
+    event.preventDefault();
+    //* create new element
     const newItem = document.createElement("li");
+    //* add a class to it
     newItem.classList.add("item");
-    newItem.innerText = `Item ${items.length + 1}`;
+    //* add text
+    newItem.innerText = nameInput.value;
     todoList.appendChild(newItem);
     todoNr.innerText = items.length;
+    // ! Deleteing the value from the input
+    nameInput.value = "";
     // ! Creating the element and attaching the listner to it
     newItem.addEventListener("click", deleteItem);
+
+    console.log(nameInput);
 });
 
 function deleteItem(event) {
     //* - which element console.log(event.target);
     //* console.log("Delete item");
-
+    event.stopPropagation(); // ! makes sure it will not bubble to other items
     event.target.remove(); // ! removes the element
 }
+
+todoList.addEventListener("click", function () {
+    todoList.classList.toggle("fade");
+});
