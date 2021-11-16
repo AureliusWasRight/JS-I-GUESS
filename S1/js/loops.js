7ea0340bbb25c2a9afaecfbfc8bbbb2073dfc616
Jakub Łapiński <jakublapinskigh@gmail.com>
2021-11-16 15:27:17 +0100

    looooooooooooops

---
+++ S1/js/loops.js
@@ -0,0 +1,17 @@
+const text = [
+    "Singing in the park",
+    "Watching mysel dance",
+    "Dogo is danger",
+    "Ayo worky",
+];
+
+/*for (i = 0; i < 100; i++) {
+    if (i === 20) {
+        break;
+    }
+    console.log(i);
+}*/
+
+for (let i = 0; i <= text.length; i++) {
+    console.log(text[i]);
+}
