b6bb3ad45381c55f75b389e14f9cd1d66b4fcbb8
Jakub Łapiński <jakublapinskigh@gmail.com>
2021-11-16 20:21:40 +0100

    EVENTS ARE SICK

--- DOM/js/events.js
+++ DOM/js/events.js
@@ -1,16 +1,29 @@
 const button = document.querySelector("#submit");
+const mainTitle = document.querySelector(".main-title");
+const secondButton = document.querySelector("#second-button");
 const todoList = document.querySelector("#todo-list");
 const todoNr = document.querySelector(".todo-nr b");
-const items = todoList.children;
+// const items = todoList.children;
 
-// Attaching an event listener
+// // Attaching an event listener
 
-button.addEventListener("click", addNewItem);
+// button.addEventListener("click", addNewItem);
 
-function addNewItem() {
-    const newItem = document.createElement("li");
-    newItem.classList.add("item");
-    newItem.innerText = `Item ${items.length + 1}`;
-    todoList.appendChild(newItem);
-    todoNr.innerText = items.length;
-}
+// function addNewItem() {
+//     const newItem = document.createElement("li");
+//     newItem.classList.add("item");
+//     newItem.innerText = `Item ${items.length + 1}`;
+//     todoList.appendChild(newItem);
+//     todoNr.innerText = items.length;
+// }
+
+button.addEventListener("click", function (event) {
+    console.log(event);
+    mainTitle.classList.toggle("color");
+});
+
+secondButton.addEventListener("keydown", function (event) {
+    if (event.keyCode === 81) {
+        mainTitle.classList.toggle("color");
+    }
+});
