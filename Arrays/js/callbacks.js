0439219db1fd424d0f7844d03d824748c012f661
Jakub Łapiński <jakublapinskigh@gmail.com>
2021-11-19 20:07:27 +0100

    array stuff

---
+++ Arrays/js/callbacks.js
@@ -0,0 +1,32 @@
+// ! Higher order functions are ones which take other functions in
+// // ! Higher order example
+// function repeater(fn) {
+//     fn();
+//     fn();
+//     fn();
+// }
+
+// function sayHello() {
+//     console.log("Hello");
+// }
+
+// repeater(sayHello);
+
+// repeater(function () {
+//     console.log("Hello, there");
+// });
+
+const videos = [
+    "Software Engineering",
+    "Stuff made stuff",
+    "FLEXBOX MASTERRACE",
+];
+
+videos.forEach(function (video) {
+    console.log("run");
+    console.log(video);
+});
+
+button.addEventListener("click", function () {
+    console.log(10);
+});
