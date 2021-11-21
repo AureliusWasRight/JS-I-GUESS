f3c91b5fa78863964aed7195c12deacc2f9af1dd
Jakub Łapiński <jakublapinskigh@gmail.com>
2021-11-21 12:45:06 +0100

    Fixes and thingies

--- Beatmaker/js/app.js
+++ Beatmaker/js/app.js
@@ -7,6 +7,7 @@ class DrumKit {
         this.hihatAudio = document.querySelector(".hihat-sound");
         this.index = 0;
         this.bpm = 150;
+        this.isPlaying = null;
     }
     activePad() {
         this.classList.toggle("active");
@@ -14,13 +15,50 @@ class DrumKit {
     repeat() {
         let step = this.index % 8;
         const activeBars = document.querySelectorAll(`.b${step}`);
+        //? Loop over the pads
+        activeBars.forEach((bar) => {
+            bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+            //! Check if active
+            if (bar.classList.contains("active")) {
+                //! check each sound
+                if (bar.classList.contains("kick-pad")) {
+                    this.kickAudio.currentTime = 0;
+                    this.kickAudio.play();
+                }
+                if (bar.classList.contains("snare-pad")) {
+                    this.snareAudio.currentTime = 0;
+                    this.snareAudio.play();
+                }
+                if (bar.classList.contains("hihat-pad")) {
+                    this.hihatAudio.currentTime = 0;
+                    this.hihatAudio.play();
+                }
+            }
+        });
         this.index++;
     }
     start() {
         const interval = (60 / this.bpm) * 1000;
-        setInterval(() => {
-            this.repeat();
-        }, interval);
+        // ! Check if it's playing
+        // ! Opposite of null
+        if (!this.isPlaying) {
+            this.isPlaying = setInterval(() => {
+                this.repeat();
+            }, interval);
+        } else {
+            // ! Clear rhe interval
+            clearInterval(this.isPlaying);
+            this.isPlaying = null;
+        }
+    }
+    updateBtn() {
+        if (!this.isPlaying) {
+            this.playBtn.innerText = "Play";
+            this.playBtn.classList.add("active");
+        } else {
+            this.playBtn.innerText = "Stop";
+            this.playBtn.classList.remove("active");
+        }
     }
 }
 
@@ -28,8 +66,12 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach((pad) => {
     pad.addEventListener("click", drumKit.activePad);
+    pad.addEventListener("animationend", function () {
+        this.style.animation = "";
+    });
 });
 
 drumKit.playBtn.addEventListener("click", function () {
     drumKit.start();
+    drumKit.updateBtn();
 });
